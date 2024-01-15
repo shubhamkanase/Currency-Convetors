b4658f1c0fbd414b2bd54806f8b220b3323f88e6
Shubham Kanase <shubhamkanase92@gmail,com>
2024-01-15 18:07:00 +0530

    currency convetor creation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='text-3xl bg-orange-500'>Currency Convetor</h1>
+     <h1 className='text-4xl bg-blue-200'>shubham kanase</h1>
+    </>
+  )
+}
+
+export default App
